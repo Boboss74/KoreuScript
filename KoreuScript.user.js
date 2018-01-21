b1389ea1a5a912f2d5a3443ef1e745c59887143e
Boboss <835901+Boboss74@users.noreply.github.com>
2018-01-21 21:15:49 +0100

    No more permission request message to get resources from appli.koreus.com

--- KoreuScript.user.js
+++ KoreuScript.user.js
@@ -11,6 +11,7 @@
 // @include      https://www.koreus.com/*
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
 // @require      https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js
+// @connect      appli.koreus.com
 // @grant        GM_getValue
 // @grant        GM_addStyle
 // @grant        GM_setValue
