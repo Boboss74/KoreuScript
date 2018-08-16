e1d19f8123b33bf9bfa2488b8c3bd4e012ced750
Boboss <835901+Boboss74@users.noreply.github.com>
2018-08-17 00:36:21 +0200

    Bump version to 0.10.0 + complete Readme

--- KoreuScript.user.js
+++ KoreuScript.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         KoreuScript
 // @namespace    Benissou/KoreuScript
-// @version      0.9.12
+// @version      0.10.0
 // @author       Benissou
 // @description  Amélioration du site Koreus.com
 // @homepage     https://www.koreus.com/modules/newbb/topic165924.html
